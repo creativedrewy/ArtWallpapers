86bdddffccc1e408a290970e7bf06f355f258250
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-04-07 11:28:09 -0700

    Time to get artsy

---
+++ app/src/main/java/com/creativedrewy/artwallpapers/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.creativedrewy.artwallpapers
+
+import android.app.WallpaperManager
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.creativedrewy.artwallpapers.ui.theme.ArtWallpapersTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            ArtWallpapersTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Button(
+                        modifier = Modifier.wrapContentSize(),
+                        onClick = {
+                            val intent = Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER)
+                            intent.putExtra(
+                                WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+                                ComponentName(this, MyWallpaperService::class.java)
+                            )
+
+                            startActivity(intent)
+                        }
+                    ) {
+                        Text(text = "Click me")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ArtWallpapersTheme {
+
+    }
+}
