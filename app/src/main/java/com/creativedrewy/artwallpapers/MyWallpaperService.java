600138a05d6fa946f73c0419061f2fad6cf57894
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-04-07 11:46:08 -0700

    Bring in webview consumption code

--- app/src/main/java/com/creativedrewy/artwallpapers/MyWallpaperService.java
+++ app/src/main/java/com/creativedrewy/artwallpapers/MyWallpaperService.java
@@ -1,138 +1,212 @@
 package com.creativedrewy.artwallpapers;
 
-import android.content.SharedPreferences;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.os.Handler;
-import android.preference.PreferenceManager;
 import android.service.wallpaper.WallpaperService;
-import android.view.MotionEvent;
+import android.util.Log;
 import android.view.SurfaceHolder;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MyWallpaperService extends WallpaperService {
+    // We can have multiple engines running at once (since you might have one on your home screen
+    // and another in the settings panel, for instance), so for debugging it's useful to keep track
+    // of which one is which. We give each an id based on this nextEngineId.
+    static int nextEngineId = 1;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 
     @Override
     public Engine onCreateEngine() {
-        return new MyWallpaperEngine();
+        return new MyEngine(this);
     }
 
-    private class MyWallpaperEngine extends Engine {
-        private final Handler handler = new Handler();
-        private final Runnable drawRunner = () -> draw();
-        private List<MyPoint> circles;
-        private Paint paint = new Paint();
-        private int width;
-        int height;
-        private boolean visible = true;
-        private int maxNumber;
-        private boolean touchEnabled;
+    // This JSInterface allows us to get messages from the WebView.
+    public class JSInterface {
+        private MyEngine myActivity;
+        public JSInterface  (MyEngine activity) {
+            myActivity = activity;
+        }
 
-        public MyWallpaperEngine() {
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MyWallpaperService.this);
-            maxNumber = 25;
-            touchEnabled = true;
-            circles = new ArrayList<>();
+        @JavascriptInterface
+        public void drawFrame(){
+            myActivity.incomingMessage();
+        }
+    }
 
-            paint.setAntiAlias(true);
-            paint.setColor(Color.WHITE);
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setStrokeJoin(Paint.Join.ROUND);
-            paint.setStrokeWidth(10f);
-            handler.post(drawRunner);
+    public class MyEngine extends Engine {
+        private Context myContext;
+        private WebView myWebView;
+        private SurfaceHolder myHolder;
+        private int myId;
+        private JSInterface myJSInterface;
+        private BroadcastReceiver myMessageReceiver;
+
+//        private final Handler handler = new Handler();
+//        private final Runnable drawRunner = this::drawFrame;
+
+        public MyEngine(Context context) {
+            myId = nextEngineId;
+            nextEngineId++;
+            myContext = context;
+            myWebView = null;
+            myMessageReceiver = null;
+            log("Engine created.");
+        }
+
+        private void log(String message) {
+            Log.d("MyLWP " + myId, message);
+        }
+
+        private void logError(String message) {
+            Log.e("MyLWP " + myId, message);
+        }
+
+        public void incomingMessage() {
+            // The message from the WebView might not be on the right thread, so we use a broadcast to fix that.
+            Intent intent = new Intent("draw-frame");
+            //LocalBroadcastManager.getInstance(myContext).sendBroadcast(intent);
+        }
+
+        @Override
+        public void onCreate(SurfaceHolder surfaceHolder) {
+            log("On Create");
+            super.onCreate(surfaceHolder);
+        }
+
+        @Override
+        public void onDestroy() {
+            log("On Destroy");
+            super.onDestroy();
         }
 
         @Override
         public void onVisibilityChanged(boolean visible) {
-            this.visible = visible;
+            log("On Visibility Changed " + String.valueOf(visible));
+            super.onVisibilityChanged(visible);
+
+            if (myWebView == null) {
+                return;
+            }
+
+            // To save battery, when we're not visible we want the WebView to stop processing,
+            // so we use the loadUrl mechanism to call some JavaScript to tell it to pause.
             if (visible) {
-                handler.post(drawRunner);
+                //myWebView.loadUrl("javascript:resumeWallpaper()");
+                drawFrame();
+                //handler.post(drawRunner);
             } else {
-                handler.removeCallbacks(drawRunner);
+                //myWebView.loadUrl("javascript:pauseWallpaper()");
+                //handler.removeCallbacks(drawRunner);
             }
         }
 
         @Override
         public void onSurfaceCreated(SurfaceHolder holder) {
+            log("On Surface Create");
             super.onSurfaceCreated(holder);
+
+            myHolder = holder;
+
+            // Create WebView
+            if (myWebView != null) {
+                myWebView.destroy();
+            }
+
+            WebViewClient client = new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                    return false;
+                }
+            };
+
+            myWebView = new WebView(myContext);
+            myWebView.setWebViewClient(client);
+
+            WebView.setWebContentsDebuggingEnabled(true);
+            WebSettings webSettings = myWebView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            webSettings.setDomStorageEnabled(true);
+            webSettings.setBlockNetworkLoads(false);
+            webSettings.setBlockNetworkImage(false);
+
+            myJSInterface = new JSInterface(this);
+            myWebView.addJavascriptInterface(myJSInterface, "androidWallpaperInterface");
+
+            myWebView.loadUrl("https://threejs.org/examples/webgl_loader_ldraw.html");
+
+            // Create message receiver
+            myMessageReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    drawFrame();
+                }
+            };
+
+//            LocalBroadcastManager.getInstance(myContext).registerReceiver(myMessageReceiver,
+//                    new IntentFilter("draw-frame")
+//            );
         }
 
         @Override
         public void onSurfaceDestroyed(SurfaceHolder holder) {
+            log("On Surface Destroy");
+
+            //handler.removeCallbacks(drawRunner);
+
+            if (myMessageReceiver != null) {
+//                LocalBroadcastManager.getInstance(myContext).unregisterReceiver(myMessageReceiver);
+                myMessageReceiver = null;
+            }
+
+            if (myWebView != null) {
+                myWebView.destroy();
+                myWebView = null;
+            }
+
             super.onSurfaceDestroyed(holder);
-            this.visible = false;
-            handler.removeCallbacks(drawRunner);
         }
 
         @Override
         public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            this.width = width;
-            this.height = height;
+            log("On Surface Changed " + String.valueOf(format) + ", " + String.valueOf(width) + ", " + String.valueOf(height));
             super.onSurfaceChanged(holder, format, width, height);
+
+            if (myWebView != null) {
+                int widthSpec = View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY);
+                int heightSpec = View.MeasureSpec.makeMeasureSpec(height, View.MeasureSpec.EXACTLY);
+                myWebView.measure(widthSpec, heightSpec);
+                myWebView.layout(0, 0, width, height);
+            }
         }
 
-        @Override
-        public void onTouchEvent(MotionEvent event) {
-            if (touchEnabled) {
-                float x = event.getX();
-                float y = event.getY();
-                SurfaceHolder holder = getSurfaceHolder();
-                Canvas canvas = null;
-
+        public void drawFrame() {
+            if (myWebView != null) {
                 try {
-                    canvas = holder.lockCanvas();
-                    if (canvas != null) {
-                        canvas.drawColor(Color.BLACK);
-                        circles.clear();
-                        circles.add(new MyPoint(String.valueOf(circles.size() + 1), x, y));
-                        drawCircles(canvas, circles);
-
-                    }
-                } finally {
-                    if (canvas != null)
-                        holder.unlockCanvasAndPost(canvas);
-                }
-
-                super.onTouchEvent(event);
-            }
-        }
-
-        private void draw() {
-            SurfaceHolder holder = getSurfaceHolder();
-            Canvas canvas = null;
-
-            try {
-                canvas = holder.lockCanvas();
-                if (canvas != null) {
-                    if (circles.size() >= maxNumber) {
-                        circles.clear();
+                    Canvas canvas = myHolder.lockCanvas();
+                    if (canvas == null) {
+                        logError("Can't lock canvas");
+                    } else {
+                        myWebView.draw(canvas);
+                        myHolder.unlockCanvasAndPost(canvas);
                     }
 
-                    int x = (int) (width * Math.random());
-                    int y = (int) (height * Math.random());
-                    circles.add(new MyPoint(String.valueOf(circles.size() + 1), x, y));
-                    drawCircles(canvas, circles);
+                    Log.v("Andrew", "Drawing frame");
+                } catch (Exception e) {
+                    logError("drawing exception " + e.toString());
                 }
-            } finally {
-                if (canvas != null)
-                    holder.unlockCanvasAndPost(canvas);
-            }
 
-            handler.removeCallbacks(drawRunner);
-            if (visible) {
-                handler.postDelayed(drawRunner, 33);
-            }
-        }
-
-        // Surface view requires that all elements are drawn completely
-        private void drawCircles(Canvas canvas, List<MyPoint> circles) {
-            canvas.drawColor(Color.GRAY);
-
-            for (MyPoint point : circles) {
-                canvas.drawCircle(point.x, point.y, 20.0f, paint);
+                //handler.removeCallbacks(drawRunner);
+                //handler.postDelayed(drawRunner, 33);
             }
         }
     }
