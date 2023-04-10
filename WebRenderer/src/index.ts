e6c524f4fbd32ee271d2d4a82a1ca93702c50bf4
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-04-10 13:26:40 -0700

    Get initial script removal in place

--- WebRenderer/src/index.ts
+++ WebRenderer/src/index.ts
@@ -1,30 +1,63 @@
 const testScript = `
-        function setup() {
-            createCanvas(400, 400);
-        }
+let rectX = 0;
+let fr = 30; //starting FPS
+let clr;
 
-        function draw() {
-            background(220);
-            ellipse(50,50,80,80);
-        }
+function setup() {
+  createCanvas(400, 400);
+
+  background(200);
+  clr = color(255, 0, 0);
+
+  frameRate(30);
+}
+
+function draw() {
+  background(200);
+  rectX += 1; // Move Rectangle
+
+  if (rectX >= width) {
+    // If you go off screen.
+    if (fr === 30) {
+      clr = color(0, 0, 255);
+      fr = 10;
+      //frameRate(fr);
+    } else {
+      clr = color(255, 0, 0);
+      fr = 30;
+      //frameRate(fr);
+    }
+    rectX = 0;
+  }
+
+  fill(clr);
+  rect(rectX, 40, 20, 20);
+}
 `
 
 window.onload = () => {
-    const firstButton = document.getElementById("testButton")
+    let userScript: HTMLScriptElement
+    let p5Script: HTMLScriptElement
+
+    const firstButton = document.getElementById("addButton")
     firstButton.onclick = () => {
 
-        let userScript = document.createElement('script')
+        userScript = document.createElement('script')
         userScript.textContent = testScript
 
         const p5Path = "https://cdn.jsdelivr.net/npm/p5@1.6.0/lib/p5.js"
-        let script = document.createElement('script');
-// script.onload = cb;
-        script.onerror = () => {
-            console.log("Failed to load script: " + p5Path);
-        };
-        script.setAttribute('src', p5Path);
+        p5Script = document.createElement('script');
+        p5Script.setAttribute('src', p5Path);
 
         document.body.appendChild(userScript);
-        document.body.appendChild(script);
+        document.body.appendChild(p5Script);
+    }
+
+    const removeButton = document.getElementById("removeButton")
+    removeButton.onclick = () => {
+        let item = document.body.getElementsByClassName("p5Canvas")[0]
+        item.remove()
+        userScript.remove()
+        p5Script.remove()
     }
 }
