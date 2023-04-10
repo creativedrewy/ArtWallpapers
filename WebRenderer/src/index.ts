395a78330d34a021d53bc95d4036bdbd9f9057bb
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-04-10 11:16:51 -0700

    Get really basic dynamic p5 loading method in place

--- WebRenderer/src/index.ts
+++ WebRenderer/src/index.ts
@@ -1,1 +1,30 @@
+const testScript = `
+        function setup() {
+            createCanvas(400, 400);
+        }
 
+        function draw() {
+            background(220);
+            ellipse(50,50,80,80);
+        }
+`
+
+window.onload = () => {
+    const firstButton = document.getElementById("testButton")
+    firstButton.onclick = () => {
+
+        let userScript = document.createElement('script')
+        userScript.textContent = testScript
+
+        const p5Path = "https://cdn.jsdelivr.net/npm/p5@1.6.0/lib/p5.js"
+        let script = document.createElement('script');
+// script.onload = cb;
+        script.onerror = () => {
+            console.log("Failed to load script: " + p5Path);
+        };
+        script.setAttribute('src', p5Path);
+
+        document.body.appendChild(userScript);
+        document.body.appendChild(script);
+    }
+}
