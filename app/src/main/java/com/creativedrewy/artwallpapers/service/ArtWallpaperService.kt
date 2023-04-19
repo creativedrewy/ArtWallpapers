88ba8b0582a0eb486c4b27d233c829efa8262fd6
creativedrewy <Andrew Watson andrew.watson@solanamobile.com>
2023-04-19 14:03:27 -0700

    Add new wallpaper service class

---
+++ app/src/main/java/com/creativedrewy/artwallpapers/service/ArtWallpaperService.kt
@@ -0,0 +1,10 @@
+package com.creativedrewy.artwallpapers.service
+
+import android.service.wallpaper.WallpaperService
+
+class ArtWallpaperService : WallpaperService() {
+
+    override fun onCreateEngine(): Engine {
+        TODO("Not yet implemented")
+    }
+}
